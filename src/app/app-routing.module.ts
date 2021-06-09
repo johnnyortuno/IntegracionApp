7879364555e9a590a8f02e51f04898e4ce78e889
jonito <jonito_or@hotmail.com>
2021-06-09 12:58:20 -0300

    Segunda Version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { BusquedaComponent } from './busqueda/busqueda.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {FichaComponent} from '../app/ficha/ficha.component'
+ 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    component: FichaComponent
+  },
+  {
+    path: 'busqueda',
+    component: BusquedaComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { DetalleComponent } from './detalle/detalle.component';
+import { FichaComponent } from './ficha/ficha.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CabeceraComponent,
+    DetalleComponent,
+    FichaComponent,
+    BusquedaComponent
   ],
   imports: [
     BrowserModule,
